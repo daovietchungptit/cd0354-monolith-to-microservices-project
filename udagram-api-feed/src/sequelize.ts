f128732796541cbf1c6a9acc404d62dceebefc47
daovietchungptit <daovietchungptit@hotmail.com>
2023-07-26 16:45:43 +0700

    Update squenlize

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -10,10 +10,10 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
-  'dialectOptions': {
-    'ssl': {
-        'require': true,
-        'rejectUnauthorized': false
-    }
-  },
+  // 'dialectOptions': {
+  //   'ssl': {
+  //       'require': true,
+  //       'rejectUnauthorized': false
+  //   }
+  // },
 });
